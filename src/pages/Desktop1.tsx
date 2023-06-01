b2dc6ea89e5a1ca356200d033b7f4b127b3ebc97
KrishnaHarish <KrishnaHarish>
2023-06-01 04:50:28 +0000

    First Commit from Locofy Builder

---
+++ src/pages/Desktop1.tsx
@@ -0,0 +1,286 @@
+import { FunctionComponent } from "react";
+import styles from "./Desktop1.module.css";
+const Desktop1: FunctionComponent = () => {
+  return (
+    <div className={styles.desktop1}>
+      <img className={styles.sasa21} alt="" src="/sasa-2-1@2x.png" />
+      <div className={styles.desktop1Child} />
+      <div className={styles.desktop1Item} />
+      <div className={styles.desktop1Inner} />
+      <div className={styles.rectangleDiv} />
+      <img className={styles.rcg1Icon} alt="" src="/rcg-1@2x.png" />
+      <b className={styles.studio}>Studio</b>
+      <div className={styles.groupParent}>
+        <div className={styles.groupGroup}>
+          <img className={styles.groupIcon} alt="" src="/group.svg" />
+          <div className={styles.dashboard}>Dashboard</div>
+        </div>
+        <div className={styles.groupWrapper}>
+          <div className={styles.vectorParent}>
+            <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+            <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+            <div className={styles.employee}>Employee</div>
+            <img className={styles.layer3Icon} alt="" src="/layer-3.svg" />
+          </div>
+        </div>
+        <div className={styles.vectorGroup}>
+          <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
+          <div className={styles.tasks}>Tasks</div>
+        </div>
+        <div className={styles.groupContainer}>
+          <div className={styles.vectorParent}>
+            <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+            <img className={styles.vectorIcon3} alt="" src="/vector3.svg" />
+            <img className={styles.vectorIcon4} alt="" src="/vector4.svg" />
+            <img className={styles.vectorIcon5} alt="" src="/vector5.svg" />
+            <img className={styles.vectorIcon6} alt="" src="/vector6.svg" />
+            <img className={styles.vectorIcon7} alt="" src="/vector7.svg" />
+            <div className={styles.documents}>Documents</div>
+          </div>
+        </div>
+        <div className={styles.vectorContainer}>
+          <img className={styles.vectorIcon2} alt="" src="/vector8.svg" />
+          <div className={styles.tasks1}>Tasks</div>
+        </div>
+        <div className={styles.vectorParent1}>
+          <img className={styles.vectorIcon9} alt="" src="/vector9.svg" />
+          <div className={styles.account}>Account</div>
+        </div>
+        <div className={styles.groupParent1}>
+          <img className={styles.groupChild} alt="" src="/group-1.svg" />
+          <div className={styles.settings}>Settings</div>
+        </div>
+        <div className={styles.vectorParent2}>
+          <img className={styles.vectorIcon10} alt="" src="/vector10.svg" />
+          <div className={styles.logOut}>Log out</div>
+        </div>
+        <div className={styles.teamParent}>
+          <div className={styles.team}>Team</div>
+          <img className={styles.groupIcon2} alt="" src="/group2.svg" />
+        </div>
+      </div>
+      <b className={styles.summary}>Summary</b>
+      <div className={styles.rectangleParent}>
+        <div className={styles.groupItem} />
+        <b className={styles.exportData}>Export Data</b>
+      </div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.groupInner} />
+        <b className={styles.q12022}>Q1 2022</b>
+      </div>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.groupChild1} />
+        <div className={styles.groupChild2} />
+        <div className={styles.groupChild3} />
+        <div className={styles.groupChild4} />
+      </div>
+      <div className={styles.maskGroup}>
+        <div className={styles.maskGroupChild} />
+        <div className={styles.maskGroupItem} />
+        <div className={styles.totalEmployees}>Total employees</div>
+        <b className={styles.b}>2,345</b>
+        <div className={styles.applicationsNeedReviewContainer}>
+          <p className={styles.applicationsNeed}>29 applications need</p>
+          <p className={styles.applicationsNeed}>review</p>
+        </div>
+        <img className={styles.maskGroupInner} alt="" src="/arrow-1.svg" />
+        <div className={styles.div}>5%</div>
+      </div>
+      <div className={styles.maskGroup1}>
+        <div className={styles.maskGroupChild} />
+        <div className={styles.maskGroupItem} />
+        <div className={styles.totalEmployees}>Total overtime</div>
+        <b className={styles.b}>765</b>
+        <div className={styles.applicationsNeedReviewContainer}>
+          <p
+            className={styles.applicationsNeed}
+          >{`45 overtime schedules need `}</p>
+          <p className={styles.applicationsNeed}>approval</p>
+        </div>
+        <img className={styles.arrowIcon} alt="" src="/arrow-1.svg" />
+        <div className={styles.div1}>25%</div>
+      </div>
+      <div className={styles.maskGroup2}>
+        <div className={styles.maskGroupChild} />
+        <div className={styles.maskGroupItem} />
+        <div className={styles.totalEmployees}>Total leave</div>
+        <b className={styles.b}>98</b>
+        <div className={styles.applicationsNeedReviewContainer}>
+          <p
+            className={styles.applicationsNeed}
+          >{`22 paid leave plans need `}</p>
+          <p className={styles.applicationsNeed}>approval</p>
+        </div>
+        <img className={styles.maskGroupInner} alt="" src="/arrow-1.svg" />
+        <div className={styles.div2}>16%</div>
+      </div>
+      <div className={styles.desktop1Child1} />
+      <img className={styles.desktop1Child2} alt="" src="/arrow-2.svg" />
+      <b className={styles.timeOffStatistics}>Time Off Statistics</b>
+      <b className={styles.employeeRanks}>Employee Ranks</b>
+      <b className={styles.b3}>
+        <p className={styles.p}>50</p>
+        <p className={styles.p}>25</p>
+        <p className={styles.p}>20</p>
+        <p className={styles.p}>15</p>
+        <p className={styles.p}>10</p>
+        <p className={styles.p}>5</p>
+        <p className={styles.applicationsNeed}>0</p>
+      </b>
+      <div className={styles.rectangleParent1}>
+        <div className={styles.groupChild5} />
+        <div className={styles.groupChild6} />
+        <div className={styles.groupChild7} />
+        <div className={styles.apr15}>Apr 15</div>
+      </div>
+      <div className={styles.rectangleParent2}>
+        <div className={styles.groupChild8} />
+        <div className={styles.groupChild9} />
+        <div className={styles.groupChild10} />
+        <div className={styles.apr16}>Apr 16</div>
+      </div>
+      <div className={styles.rectangleParent3}>
+        <div className={styles.groupChild11} />
+        <div className={styles.groupChild12} />
+        <div className={styles.groupChild13} />
+        <div className={styles.apr17}>Apr 17</div>
+      </div>
+      <div className={styles.rectangleParent4}>
+        <div className={styles.groupChild14} />
+        <div className={styles.groupChild15} />
+        <div className={styles.groupChild16} />
+        <div className={styles.apr18}>Apr 18</div>
+      </div>
+      <div className={styles.rectangleParent5}>
+        <div className={styles.groupChild17} />
+        <div className={styles.groupChild18} />
+        <div className={styles.groupChild19} />
+        <div className={styles.apr19}>Apr 19</div>
+      </div>
+      <div className={styles.rectangleParent6}>
+        <div className={styles.groupChild20} />
+        <div className={styles.groupChild21} />
+        <div className={styles.groupChild22} />
+        <div className={styles.apr16}>Apr 20</div>
+      </div>
+      <div className={styles.rectangleParent7}>
+        <div className={styles.groupChild23} />
+        <div className={styles.sickLeave}>Sick Leave</div>
+      </div>
+      <div className={styles.rectangleParent8}>
+        <div className={styles.groupChild24} />
+        <div className={styles.sickLeave}>Paid Leave</div>
+      </div>
+      <div className={styles.rectangleParent9}>
+        <div className={styles.groupChild25} />
+        <div className={styles.sickLeave}>Unpaid Leave</div>
+      </div>
+      <div className={styles.lineDiv} />
+      <div className={styles.desktop1Child3} />
+      <div className={styles.desktop1Child4} />
+      <div className={styles.desktop1Child5} />
+      <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+      <div className={styles.mochRasheed}>Moch. Rasheed</div>
+      <div className={styles.raffee}>Raffee</div>
+      <div className={styles.richman}>Richman</div>
+      <div className={styles.raffee1}>Raffee</div>
+      <div className={styles.uiDesigner}>UI Designer</div>
+      <div className={styles.graphicsDesigner}>Graphics Designer</div>
+      <div className={styles.arabicTranslator}>Arabic Translator</div>
+      <div className={styles.fullTime}>Full time - Remote</div>
+      <div className={styles.fullTime1}>Full time - Remote</div>
+      <div className={styles.fullTime2}>Full time - Remote</div>
+      <div className={styles.ellipseDiv} />
+      <div className={styles.div3}>100 %</div>
+      <div className={styles.div4}>80 %</div>
+      <div className={styles.div5}>75 %</div>
+      <div className={styles.taskCompleted}>Task Completed</div>
+      <div className={styles.taskCompleted1}>Task Completed</div>
+      <div className={styles.taskCompleted2}>Task Completed</div>
+      <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+      <img className={styles.image3Icon} alt="" src="/image-2@2x.png" />
+      <img className={styles.subtractIcon} alt="" src="/subtract.svg" />
+      <img className={styles.subtractIcon} alt="" src="/subtract.svg" />
+      <div className={styles.upcomingSchedule}>Upcoming Schedule</div>
+      <div className={styles.april2022}>April 2022 - Week 3</div>
+      <div className={styles.div6}>15</div>
+      <div className={styles.div7}>16</div>
+      <div className={styles.div8}>17</div>
+      <div className={styles.div9}>18</div>
+      <div className={styles.div10}>19</div>
+      <div className={styles.div11}>20</div>
+      <div className={styles.div12}>21</div>
+      <div className={styles.fri}>Fri</div>
+      <div className={styles.sat}>Sat</div>
+      <div className={styles.sun}>Sun</div>
+      <div className={styles.mon}>Mon</div>
+      <div className={styles.tue}>Tue</div>
+      <div className={styles.wed}>Wed</div>
+      <div className={styles.thu}>Thu</div>
+      <div className={styles.desktop1Child6} />
+      <div className={styles.desktop1Child7} />
+      <div className={styles.desktop1Child8} />
+      <div className={styles.desktop1Child9} />
+      <div className={styles.desktop1Child10} />
+      <div className={styles.desktop1Child11} />
+      <img className={styles.desktop1Child12} alt="" src="/arrow-3.svg" />
+      <img className={styles.desktop1Child13} alt="" src="/arrow-4.svg" />
+      <div className={styles.bussinessTrip}>BUSSINESS TRIP</div>
+      <div className={styles.paidLeave1}>PAID LEAVE</div>
+      <img className={styles.vectorIcon11} alt="" src="/vector11.svg" />
+      <img className={styles.vectorIcon12} alt="" src="/vector12.svg" />
+      <img className={styles.vectorIcon13} alt="" src="/vector13.svg" />
+      <img className={styles.vectorIcon14} alt="" src="/vector13.svg" />
+      <div className={styles.desktop1Child14} />
+      <div className={styles.cashflow}>Cashflow</div>
+      <div className={styles.income}>Income</div>
+      <div className={styles.outcome}>Outcome</div>
+      <div className={styles.desktop1Child15} />
+      <div className={styles.desktop1Child16} />
+      <b className={styles.statistic}>Statistic</b>
+      <b className={styles.news}>News</b>
+      <img className={styles.customer21} alt="" src="/customer-2-1@2x.png" />
+      <div className={styles.translator}>Translator</div>
+      <div className={styles.graphicsDesigner1}>Graphics Designer</div>
+      <div className={styles.softwareEngineer}>Software Engineer</div>
+      <div className={styles.gameDesigner}>{`Game Designer `}</div>
+      <div className={styles.uiDesigner1}>UI Designer</div>
+      <div className={styles.div13}>11.5 %</div>
+      <div className={styles.div14}>26.9 %</div>
+      <div className={styles.div15}>23.1 %</div>
+      <div className={styles.div16}>7.7 %</div>
+      <div className={styles.div17}>30.8 %</div>
+      <div className={styles.desktop1Child17} />
+      <div className={styles.desktop1Child18} />
+      <div className={styles.desktop1Child19} />
+      <div className={styles.desktop1Child20} />
+      <div className={styles.desktop1Child21} />
+      <div className={styles.maskGroup3}>
+        <img className={styles.image4Icon} alt="" src="/image-4@2x.png" />
+        <div className={styles.maskGroupChild6} />
+        <b className={styles.howDoWe}>How Do We Stay Focus?</b>
+      </div>
+      <div className={styles.div18}>9</div>
+      <div className={styles.text} />
+      <div className={styles.div19}>1</div>
+      <div className={styles.div20}>0</div>
+      <div className={styles.text1} />
+      <div className={styles.div21}>1</div>
+      <div className={styles.div22}>1</div>
+      <div className={styles.text2} />
+      <div className={styles.div23}>1</div>
+      <div className={styles.div24}>2</div>
+      <div className={styles.text3} />
+      <div className={styles.div25}>1</div>
+      <div className={styles.div26}>3</div>
+      <div className={styles.text4} />
+      <div className={styles.div27}>1</div>
+      <div className={styles.div28}>4</div>
+      <div className={styles.text5} />
+      <div className={styles.div29}>1</div>
+      <div className={styles.div30}>5</div>
+    </div>
+  );
+};
+
+export default Desktop1;
